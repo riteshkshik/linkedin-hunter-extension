800db99d90ae79d88f0d0ad1249a898802b5ac9c
riteshkshik <stevenfrayne12344@gmail.com>
2025-09-07 13:56:07 +0530

    removed api key exposure and added compressed video

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Replace 'YOUR_API_KEY_HERE' with your Hunter.io API key
-const HUNTER_API_KEY = "c03488c84c06ff6055afca2e4546835e7fe7c172";
+const HUNTER_API_KEY = process.env.HUNTER_API_KEY || "YOUR_API_KEY_HERE";
